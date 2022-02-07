a4e1fcd1887051db87527c979b2a4a15da10e5db
Victor Laurentino <victorlaurentino7@gmail.com>
2022-02-07 10:43:44 -0300

    Update routes

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:payflow/shared/auth/auth_controller.dart';
+import 'package:payflow/shared/models/user_model.dart';
 
 class LoginController {
   final authController = AuthController();
@@ -9,7 +10,9 @@ class LoginController {
     GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ["email"]);
     try {
       final response = await _googleSignIn.signIn();
-      authController.setUser(context, response);
+      final user = UserModel(
+          name: response!.displayName!, photoURL: response.photoUrl);
+      authController.setUser(context, user);
       print(response);
     } catch (error) {
       authController.setUser(context, null);
